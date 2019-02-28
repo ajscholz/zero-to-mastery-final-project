500d0290436d15b635ac6af9dae5f31c177cfbff
Andrew Scholz <yoitsscholzy@gmail.com>
2019-02-27 21:48:26 -0500

    updating for onling hosting

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://aqueous-tundra-39972.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
